ef29d2fa9161adcaafe826c2838a8c22ff3f7309
Muhammad Dio Revansa <mhdiorevansa098@gmail.com>
2025-12-15 21:37:47 +0700

    make crud in user controller

--- app/http/controllers/user_controller.go
+++ app/http/controllers/user_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"goravel/app/models"
 
 	"github.com/goravel/framework/contracts/http"
@@ -19,7 +20,6 @@ func NewUserController() *UserController {
 
 func (r *UserController) Show(ctx http.Context) http.Response {
 	var users []models.User
-
 	err := facades.Orm().Query().Select("id", "name", "email").Get(&users)
 	if err != nil {
 		return ctx.Response().Json(500, map[string]any {
@@ -27,9 +27,96 @@ func (r *UserController) Show(ctx http.Context) http.Response {
 			"message": err.Error(),
 		})
 	}
-
 	return ctx.Response().Json(200, map[string]any {
 		"success": true,
+		"message": "success fetch data",
 		"data": users,
+	})
+}
+
+func (r *UserController) Detail(ctx http.Context) http.Response {
+	id := ctx.Request().Route("id")
+	var user models.User
+	err := facades.Orm().Query().Select("id", "name", "email").Where("id", id).First(&user)
+	if err != nil {
+		return ctx.Response().Json(500, map[string]any {
+			"success": false,
+			"message": err.Error(),
+		})
+	}
+	return ctx.Response().Json(200, map[string]any {
+		"success": true,
+		"message": "success fetch data",
+		"data": user,
+	})
+}
+
+func (r *UserController) Create(ctx http.Context) http.Response {
+	var user models.User
+	err := ctx.Request().Bind(&user)
+	if err != nil {
+		return ctx.Response().Json(500, map[string]any {
+			"success": false,
+			"message": err.Error(),
+		})
+	}
+	password := user.Password
+	hashedPassword, err := facades.Hash().Make(password)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	user.Password = hashedPassword
+	err = facades.Orm().Query().Create(&user)
+	if err != nil {
+		return ctx.Response().Json(500, map[string]any {
+			"success": false,
+			"message": err.Error(),
+		})
+	}
+	return ctx.Response().Json(200, map[string]any {
+		"success": true,
+		"message": "success create data",
+	})
+}
+
+func (r *UserController) Update(ctx http.Context) http.Response {
+	var user models.User
+	id := ctx.Request().Route("id")
+	err := ctx.Request().Bind(&user)
+	if err != nil {
+		return ctx.Response().Json(500, map[string]any {
+			"success": false,
+			"message": err.Error(),
+		})
+	}
+	password := user.Password
+	hashedPassword, err := facades.Hash().Make(password)
+	user.Password = hashedPassword
+	_, err = facades.Orm().Query().Where("id", id).Update(&user)
+	if err != nil {
+		return ctx.Response().Json(500, map[string]any {
+			"success": false,
+			"message": err.Error(),
+		})
+	}
+	return ctx.Response().Json(200, map[string]any {
+		"success": true,
+		"message": "success update data",
+	})
+}
+
+func (r *UserController) Delete(ctx http.Context) http.Response {
+	id := ctx.Request().Route("id")
+	var user models.User
+	_, err := facades.Orm().Query().Where("id", id).Delete(&user)
+	if err != nil {
+		return ctx.Response().Json(500, map[string]any {
+			"success": false,
+			"message": err.Error(),
+		})
+	}
+	return ctx.Response().Json(200, map[string]any{
+		"success": false,
+		"message": "success delete data",
 	})
 }
--- routes/api.go
+++ routes/api.go
@@ -9,4 +9,8 @@ import (
 func Api() {
 	userController := controllers.NewUserController()
 	facades.Route().Get("/users", userController.Show)
+	facades.Route().Prefix("user").Get("/{id}", userController.Detail)
+	facades.Route().Prefix("user").Post("/create", userController.Create)
+	facades.Route().Prefix("user").Put("/update/{id}", userController.Update)
+	facades.Route().Prefix("user").Delete("/delete/{id}", userController.Delete)
 }
